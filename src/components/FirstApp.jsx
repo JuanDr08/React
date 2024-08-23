27da2584607e635f39850c337f40208c963e0bcc
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-08-23 12:32:19 -0500

    feat: :sparkles: Styles, props, js in html

--- src/components/FirstApp.jsx
+++ src/components/FirstApp.jsx
@@ -1,4 +1,32 @@
-export const FirstApp = () => {
+// React recomienda que el javascript que no va a tener que estarse cargando continuamente en el componente lo coloquemos por fuera del componente,
+// es decir, por estas zonas, mayormente cuando dichas funciones o codigo javascript no depende de lo que haya internamente del funcional component,
+// dado el caso que una funcion o algo de js dependa de lo que tenemos dentro del functional component entonces ahi si deberiamos pasarlo ahi dentro
+
+const pruebaProp = () => {
+    return "Se completó"
+}
+
+export const FirstApp = ({title, number}) => { // Los ' parametros ' (en realidad son props) basicamente son propiedades que se le pueden pasar al componente hijo
+    // para ser usadas en la renderizacion del componente, los props se le pasan al componente hijo como atributos al momento de crear su etiqueta en algun componente padre
+    // Los props al llegar al componente que los recibe le llegan como un objeto, pero de la forma en la que lo tengo plazmado estan siendo desestructurados directamente
+    /*
+        Los props se suelen asignar de la siguiente manera en el componente padre: <FirstApp title="Un titulo" number="123" />
+
+        Si llegado el caso no se le llega a pasar un prop de los que hemos definido en el hijo, esto podria llegar a quedar como undefined, pero para esto si queremos podemos
+        asignar valores por defecto si llegado el caso no se llegan a pasar, esto se hace asi
+        {title="Titulo por defecto", number=123} esto hara que si llegado el caso en las props no le pasamos el titulo o el numero, se asignaran esos que les pusimos de defecto
+        Por ejemplo: 
+        <FirstApp number="123" /> solo le pasamos el number entonces se asignara por defecto el titulo que pusimos
+
+        - Como number lo logico es que esperará un numero y no un string podemos especificar de una manera especial una propiedad sin tener que usar las ""
+        <FirstApp number={123} /> con las llaves es como si le estuvieramos pasando algun tipo de expresion de javascript o algun tipo de dato diferente a una cadena
+        de esta manera el prop number almacenaria un entero y no un string, y de title pondria el por defecto ya que no se especifico
+    */ 
+
+    let hola ={ // Objeto que podemos usar para ostrar en el HTML
+        nombre: "juan"
+    }
+    
     return (
         // React tiene una regla en los componentes y es que siempre se debe regresar un unico nodo padre, es decir, siempre debe regresar un unico elemento, por ejemplo
         /*
@@ -33,7 +61,12 @@ export const FirstApp = () => {
             </>
         */
         <>
-            <h1> FirstApp </h1>
+            {/* Para poder renderizar variables en html usamos las ' {} ' y dentro van las expresiones JS que queramos, menos objetos, ya que objetos directos
+                no podemos imprimir, tendriamos que acceder con el ' . ' a sus campos o serializarlo con JSON.stringify()
+            */}
+            <h1> { hola.nombre.toLocaleUpperCase() } </h1>
+            <h1> { title } </h1> {/* Accedemos a las propiedades pasadas por los props */}
+            <p> { number } </p>
             <p>Soy un parrafo</p>
         </>
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactoDOM from 'react-dom/client' // Importaciones requeridas para render
 import {App} from './components/HelloWorld' // En el archivo main por lo general no solemos crear ningun functional component, sino que debemos importar los que creamos
 // en otros archivos y de ahi renderizarlos
 import { FirstApp } from './components/FirstApp';
-
+import './css/style.css'; // importacion de archivos que serán globales
 
 ReactoDOM.createRoot( document.getElementById('root') ).render( // Basicamente seleccionamos el elemento root en donde se cargara toda nuestra aplicacion react y llamamos a la funcion render
     // para renderizar el functional component (componente en una funcion) que acabamos de crear
@@ -13,6 +13,8 @@ ReactoDOM.createRoot( document.getElementById('root') ).render( // Basicamente s
 
     // Llamamos a nuestra App como componente y la renderizamos en el elemento con id 'root' en el index.html
     <React.StrictMode> 
-        <FirstApp />
+        <FirstApp title="Titulo de un prop" number={1 + 2} /> {/* Pasamos los props como atributos a la etiqueta hija, y en el number pasamos un valor personalizado diferente
+            a un string, pueden ser expresiones js, variables, funciones etc...
+        */}
     </React.StrictMode>
 )
