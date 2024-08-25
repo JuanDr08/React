8d438b02831afbac172db61873289b4f8b0e888a
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-08-24 22:45:05 -0500

    feat: :construction: Http querys

--- src/GiftAppComponents/GifGrid.jsx
+++ src/GiftAppComponents/GifGrid.jsx
@@ -1,6 +1,26 @@
 
+/* const getGifs = async (category) => { // Este tipo de funciones no debe de ir dentro del componente, ya que cada vez que el componente vuelva a ejecutarse volverá a ejecutarse la funcion
+    y por consecuencia volverá a realizar la peticion http
+    Esta funcion lo unico que requiere del componente es la categoria, pero esta podemos pasarla a traves de los parametros, por lo que podemos permitirnos mover la funcion fuera del componente
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=8IbCERnhkF09iUxVbuFL9fTYbTt7OZpq&q=${ category }&limit=20`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => (
+        {
+            id: img.id,
+            title: img.title,
+            url: img.images.downsized_medium.url
+        }) )
+
+} */
+import { getGifs } from "./helpers/getGifs" // Pero al ser javascript, podemos separar las querys en un archivo externo e importar sus funciones para ser usadas aca en el componente
+
 
 export const GifGrid = ({ category }) => {
+
+    getGifs(category)
 
     return (
         <>
